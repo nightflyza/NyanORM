1885c6819daccdedc848306d4b8682bed4d2fc05
Nightfly <nightfly@email.ua>
2019-12-17 04:06:50 +0200

    basically backported from Ubilling

---
+++ api/api.autolader.php
@@ -0,0 +1,43 @@
+<?php
+//preloading of some required libs
+
+/*
+ * Including all needed APIs and Libs
+ */
+
+include('api/libs/api.mysql.php');
+include('api/libs/api.nyanorm.php');
+
+
+//Register and load file classes 
+spl_autoload_register(function ($className) {
+
+    $api_directory = 'api' . DIRECTORY_SEPARATOR;
+    $libs_directory = 'libs' . DIRECTORY_SEPARATOR;
+
+    // Defined path
+    $classFileName = strtolower($className);
+
+    $apiClassFileName = $api_directory . $libs_directory . 'api.' . $classFileName . '.php';
+    
+    if (strpos($className, 'nya_') !== false) {
+        $notOrmTable = str_replace("nya_", '', $className);
+
+        $exec = '
+            class ' . $className . ' extends NyanORM {
+                public function __construct() {
+                  parent::__construct();
+                   $this->tableName = "' . $notOrmTable . '";
+                 }
+             }';
+
+        eval($exec); //automatic models generation
+    } else {
+        if (file_exists($apiClassFileName)) {
+            include $apiClassFileName;
+        } elseif (file_exists($vendorClassFileName)) {
+            include $vendorClassFileName;
+        }
+    }
+});
+?>
--- sample.php
+++ sample.php
@@ -1,3 +1,9 @@
 <?php
 
+error_reporting(E_ALL);
+include ('api/autoloader.php');
 
+$todos=new nya_todo();
+$allTodos=$todos->getAll();
+
+print_r($allTodos);
