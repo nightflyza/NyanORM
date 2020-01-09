b8db83fcff38648456474a608cc15f1a3069a9d8
Nightfly <nightfly@email.ua>
2020-01-09 19:29:32 +0200

    lolz

--- cli_routing_sample.php
+++ cli_routing_sample.php
@@ -9,7 +9,7 @@ include ('api/autoloader.php');
 
 $appName = ubRouting::optionCliMe();
 $usageNotice = 'Usage: ' . $appName . ' --option1=somedata --option2=anotherdata' . "\n";
-if (ubRouting::optionCliCount() < 4) {
+if (ubRouting::optionCliCount() >= 3) {
     if (ubRouting::optionCliCheck(array('option1', 'option2'))) {
         print('option1 value: ' . ubRouting::optionCli('option1', 'raw') . "\n");
         print('option2 value: ' . ubRouting::optionCli('option2', 'raw') . "\n");
